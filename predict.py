36d7892c9ec79585d20de80b68cede524e12e3c6
aminecharfeddine <85786567+aminecharfeddine@users.noreply.github.com>
2026-01-13 14:18:34 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,6 @@
 # predict.py
 # =========================
 import joblib
-import pandas as pd
 from preprocessing import load_and_preprocess
 
 MODEL_PATH = "models/keratoconus_multiclass_pipeline.pkl"
@@ -16,13 +15,22 @@ LABEL_MAP = {
 }
 
 def predict_from_file(uploaded_file):
-    X = load_and_preprocess(uploaded_file)
+    eyes_data = load_and_preprocess(uploaded_file)
 
-    proba = model.predict_proba(X)[0]
-    pred_class = int(proba.argmax())
+    results = {}
 
-    return {
-        "class": pred_class,
-        "label": LABEL_MAP[pred_class],
-        "probabilities": proba.tolist()
-    }
+    for eye, X in eyes_data.items():
+        proba = model.predict_proba(X)[0]
+        pred_class = int(proba.argmax())
+
+        results[eye] = {
+            "class": pred_class,
+            "label": LABEL_MAP[pred_class],
+            "probabilities": {
+                "Normal": proba[0],
+                "Fruste": proba[1],
+                "Kératocône": proba[2]
+            }
+        }
+
+    return results
